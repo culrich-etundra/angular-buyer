b49b216ebc4f4bd9cd7c598440f73e8c424a6bc6
culrich-etundra <culrich@etundra.com>
2017-12-27 14:50:29 -0600

    Update home.controller.js

--- src/app/home/js/home.controller.js
+++ src/app/home/js/home.controller.js
@@ -19,18 +19,6 @@ function HomeController(ocAppName, FeaturedProducts) {
 				Title: ocAppName.Watch(),
 				SubText: 'Welcome to the ' + ocAppName.Watch() + ' application',
 				ID: 0
-			},
-			{
-				URL: 'assets/images/carousel2.jpg',
-				Title: 'Mill Hardware - Worldwide Distributor of Replacement Parts',
-				SubText: 'We supply high quality, reliable parts at extremely competitive prices.',
-				ID: 1
-			},
-			{
-				URL: 'assets/images/carousel3.jpg',
-				Title: 'Start Purchasing Today!',
-				SubText: 'In-stock orders placed by 3:30 P.M. ship the same day.',
-				ID: 2
 			}
 		]
 	};
